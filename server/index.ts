d3bd3d528383fea838c7a208b972ab9e5be5aac0
apolyeti <azhandarveen@gmail.com>
2023-07-03 14:56:49 -0700

    fix porting

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ io.on('connection', (socket: Socket) => {
 });
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
