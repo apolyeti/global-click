35617ea9952b6ace7099111be1e0c071ca51a8e7
apolyeti <azhandarveen@gmail.com>
2023-07-03 14:53:17 -0700

    fix package.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import { VStack } from "@chakra-ui/react"
-import Counter from "@components/Counter"
+import Counter from "../components/Counter"
 import { Center, Text} from '@chakra-ui/react'
 import { io, Socket } from 'socket.io-client';
 const socket: Socket = io('http://localhost:3000'); // Replace with your server URL
