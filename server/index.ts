50eddd13941b4488ce5be3aa616361de29fcebac
apolyeti <azhandarveen@gmail.com>
2023-07-03 14:00:24 -0700

    get rid of commented out code

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,9 @@
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
-// import next from 'next';
-
 
 const app = express();
 const server = http.createServer(app);
-
-// const dev = process.env.NODE_ENV !== 'production';
-// const nextApp = next({ dev });
-// const nextHandler = nextApp.getRequestHandler();
-
 const io = new Server(server);
 let globalNumber: number = 0;
 
@@ -28,9 +21,6 @@ io.on('connection', (socket: Socket) => {
     });
 });
 
-// app.get("*", (req, res) => {
-//     return nextHandler(req, res);
-// });
 
 const PORT = 3000;
 server.listen(PORT, () => {
