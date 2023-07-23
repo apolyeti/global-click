c22750961d6ed7c79a8ee59f991986d3b05a1da9
apolyeti <azhandarveen@gmail.com>
2023-07-22 18:51:49 -0700

    update next config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "http://localhost:8080/api/v1/:path*",
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
